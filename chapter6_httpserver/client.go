da77e133618692446d227a09b4b719841a328d22
Norifusa Tachiyanagi <wonderfulboyx@gmail.com>
2021-08-05 21:43:21 +0900

    パイプライニングの実装

--- chapter6_httpserver/client.go
+++ chapter6_httpserver/client.go
@@ -3,78 +3,55 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"math"
 	"net"
 	"net/http"
 	"net/http/httputil"
-	"strconv"
 )
 
-const ResponseMaxSize = math.MaxInt64
+var sendMessages = []string{
+	"kani", "ebi", "fuga", "hoge",
+}
 
 func main() {
 	addr := "localhost:8888"
-	conn, err := net.Dial("tcp", addr)
+	var conn net.Conn
+	var err error
+	requests := make([]*http.Request, 0, len(sendMessages))
+
+	conn, err = net.Dial("tcp", addr)
 	if err != nil {
 		panic(err)
 	}
-	defer func() {
-		println("fuga")
-		conn.Close()
-	}()
+	defer conn.Close()
 
-	request, err := http.NewRequest(
-		"POST",
-		"http://"+addr,
-		nil,
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	err = request.Write(conn)
-	if err != nil {
-		panic(err)
-	}
-	reader := bufio.NewReader(conn)
-	response, err := http.ReadResponse(reader, request)
-	if err != nil {
-		panic(err)
-	}
-
-	dump, err := httputil.DumpResponse(response, false)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
-
-	if len(response.TransferEncoding) < 1 ||
-		response.TransferEncoding[0] != "chunked" {
-		panic("wrong transfer encoding")
-	}
-
-	for {
-		sizeStr, err := reader.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			panic(err)
-		}
-
-		size, err := strconv.ParseInt(string(sizeStr[:len(sizeStr)-2]), 16, 64)
-		if size == 0 {
-			println("hoge")
-			break
+	for i, sendMessage := range sendMessages {
+		isLastMessage := i == len(sendMessages)-1
+		request, err := http.NewRequest(http.MethodGet, "http://"+addr+"?message="+sendMessage, nil)
+		if isLastMessage {
+			request.Header.Add("Connection", "close")
+		} else {
+			request.Header.Add("Connection", "keep-alive")
 		}
 		if err != nil {
 			panic(err)
 		}
-
-		line:= make([]byte, int(size))
-		io.ReadFull(reader, line)
-		reader.Discard(2)
-		fmt.Printf( "  %d bytes: %s\n", size, string(line))
+		err = request.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+		requests = append(requests, request)
 	}
-	println("hoge")
+
+	reader := bufio.NewReader(conn)
+	for _, request := range requests {
+		response, err := http.ReadResponse(reader, request)
+		if err != nil {
+			panic(err)
+		}
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+	}
 }
